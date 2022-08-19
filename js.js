352908be88caaa033a9fb146e3995bd024a52924
Lab04R01PC03 <mansoor21st@gmail.com>
2022-08-19 19:07:31 +0500

    aur bhae

---
+++ js.js
@@ -0,0 +1,9 @@
+let val = document.querySelector('#txt')
+
+function check() {
+    var txt = document.getElementById("txt").ariaValueMax;
+    if(txt % 2)
+    {
+        alert("Value is Even");
+    }
+}
